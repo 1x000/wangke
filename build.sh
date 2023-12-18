548c55f90be8b08c6a84f5b9106e898bfef8b0f4
1x000 <huaweilaomke402@gmail.com>
2023-12-18 19:37:28 +0700

    构建文件最终更新

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-GOARCH=amd64 GOOS=windows go build -o ./build/wangke_windows_amd64 main.go
+GOARCH=amd64 GOOS=windows go build -o ./build/wangke_windows_amd64.exe main.go
 GOARCH=amd64 GOOS=linux go build -o ./build/wangke_linux_amd64 main.go
 GOARCH=arm64 GOOS=linux go build -o ./build/wangke_linux_arm64 main.go
