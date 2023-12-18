bf3d20e2e382baacbb1011c1d1ec27a27be334a4
1x000 <huaweilaomke402@gmail.com>
2023-12-18 19:54:21 +0700

    更新构架文件

--- build.sh
+++ build.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
-GOARCH=amd64 GOOS=windows go build -o ./build/wangke_windows_amd64.exe main.go
-GOARCH=amd64 GOOS=linux go build -o ./build/wangke_linux_amd64 main.go
-GOARCH=arm64 GOOS=linux go build -o ./build/wangke_linux_arm64 main.go
+GOARCH=amd64 GOOS=windows go build -ldflags="-s -w" -o ./build/wangke_windows_amd64.exe main.go
+# 使用 UPX 压缩
+upx -9 ./build/wangke_windows_amd64.exe
+
+GOARCH=amd64 GOOS=linux go build -ldflags="-s -w" -o ./build/wangke_linux_amd64 main.go
+# 使用 UPX 压缩
+upx -9 ./build/wangke_linux_amd64
+
+GOARCH=arm64 GOOS=linux go build -ldflags="-s -w" -o ./build/wangke_linux_arm64 main.go
+# 使用 UPX 压缩
+upx -9 ./build/wangke_linux_arm64
