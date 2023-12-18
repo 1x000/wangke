c4fc1a81d5cac3b564308d0c7e88d7867efe56d6
1x000 <huaweilaomke402@gmail.com>
2023-12-18 19:59:37 +0700

    最后修改构建文件的问题

--- build.sh
+++ build.sh
@@ -1,12 +1,4 @@
 #!/bin/bash
 GOARCH=amd64 GOOS=windows go build -ldflags="-s -w" -o ./build/wangke_windows_amd64.exe main.go
-# 使用 UPX 压缩
-upx -9 ./build/wangke_windows_amd64.exe
-
 GOARCH=amd64 GOOS=linux go build -ldflags="-s -w" -o ./build/wangke_linux_amd64 main.go
-# 使用 UPX 压缩
-upx -9 ./build/wangke_linux_amd64
-
 GOARCH=arm64 GOOS=linux go build -ldflags="-s -w" -o ./build/wangke_linux_arm64 main.go
-# 使用 UPX 压缩
-upx -9 ./build/wangke_linux_arm64
